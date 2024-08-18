d82c72e062015cec8a8f7f5ef76eff8e9972333f
Durl.Dev <92341694+Durl99@users.noreply.github.com>
2024-08-17 20:23:28 -0600

    Index.js Nitori AI

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('LoliBot-MD', {
+say('Nitori AI', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`by: elrebelde21`, {
+say(`by: Durl.Dev`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
